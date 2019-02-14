9f503ef3f1888af050ad2c429d1139f7ab607a68
Frank Paulo Filho <me@frankpf.com>
2019-02-14 11:35:27 -0200

    Fix overload types of left and right

--- src/result.ts
+++ src/result.ts
@@ -7,11 +7,13 @@ export type AsyncResult<T, E = never> = Promise<
 >
 
 export function left<E extends string, T = never>(value: E): Result<T, E>
+export function left<E, T = never>(value: E): Result<T, E>
 export function left<E, T = never>(value: E): Result<T, E> {
 	return purify.Left(value)
 }
 
 export function right<T extends string, E = never>(value: T): Result<T, E>
+export function right<T, E = never>(value: T): Result<T, E>
 export function right<T, E = never>(value: T): Result<T, E> {
 	return purify.Right(value)
 }
