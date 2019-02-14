e04480bc39e276ebb4d1b097bd9e3896b078d07c
Frank Paulo Filho <me@frankpf.com>
2019-02-14 14:41:59 -0200

    Add void overload to result/ok

--- src/result.ts
+++ src/result.ts
@@ -14,6 +14,10 @@ export function err<E, T = never>(value: E): Result<T, E> {
 
 export function ok<T extends string, E = never>(value: T): Result<T, E>
 export function ok<T, E = never>(value: T): Result<T, E>
-export function ok<T, E = never>(value: T): Result<T, E> {
+export function ok<E = never>(): Result<void, E>
+export function ok<T, E = never>(value?: T): Result<T, E> {
+	if (!value) {
+		return purify.Right(undefined) as Result<void, E> as any
+	}
 	return purify.Right(value)
 }
