29735f45622a467366c75de101c2d5fbc35b2525
Frank Paulo Filho <me@frankpf.com>
2019-02-27 14:54:34 -0300

    Simplify converters/decoder types

--- src/converters/decoder.ts
+++ src/converters/decoder.ts
@@ -1,28 +1,16 @@
 import {ServiceDef} from '../types'
-import {AsyncResult, Result} from '../result'
+import {Result} from '../result'
 import {Decoders, DecoderUris} from './index'
 
-type Fn = (...args: any[]) => any
-type AsyncFn = (...args: any[]) => AsyncResult<any, any>
+type Fn = (args: any[]) => any
 
-type ExtractGenerics<F extends AsyncFn> = F extends AsyncResult<
-	infer T,
-	infer E
+type ExtractGenerics<F extends Fn> = ReturnType<F> extends Promise<
+	Result<infer T, infer E>
 >
 	? [T, E]
 	: never
-type ExtractResultT<F extends Fn> = F extends (
-	...args: any[]
-) => Promise<Result<infer T, any>>
-	? T
-	: never
-type ExtractResultE<F extends Fn> = F extends (
-	...args: any[]
-) => Promise<Result<any, infer E>>
-	? E
-	: never
 
-export type WithDecoder2<
+export type WithDecoder<
 	Service extends ServiceDef<Service>,
 	Name extends DecoderUris
 > = {
@@ -33,21 +21,6 @@ export type WithDecoder2<
 			Decoders<
 				ExtractGenerics<Service[K]>[0],
 				ExtractGenerics<Service[K]>[1]
-			>[Name]['decode']
-		>
-	>
-}
-export type WithDecoder<
-	Service extends ServiceDef<Service>,
-	Name extends DecoderUris
-> = {
-	[K in keyof Service]: (
-		...args: Parameters<Service[K]>
-	) => Promise<
-		ReturnType<
-			Decoders<
-				ExtractResultT<Service[K]>,
-				ExtractResultE<Service[K]>
 			>[Name]['decode']
 		>
 	>
